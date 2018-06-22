856b1dd7f797621294a903a8c4e5827ad19acbbb
Tey Liang Tai <tey_liang_tai@tech.gov.sg>
2018-06-22 14:54:26 +0800

    change icons and color for accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,27 +1,27 @@
-$(document).ready(function(){
-    $(".bp-accordion-set > a").on("click", function() {
+$(document).ready(function () {
+    $(".bp-accordion-set > a").on("click", function () {
         if ($(this).hasClass("active")) {
             $(this).removeClass("active");
             $(this)
                 .siblings(".bp-accordion-body")
-                .slideUp(200);
+                .slideUp(300);
             $(".bp-accordion-set > a i")
-                .removeClass("bp-icon-cross")
-                .addClass("bp-icon-plus");
+                .removeClass("bp-icon-angle-up")
+                .addClass("bp-icon-angle-down");
         } else {
             $(".bp-accordion-set > a i")
-                .removeClass("bp-icon-cross")
-                .addClass("bp-icon-plus");
+                .removeClass("bp-icon-angle-up")
+                .addClass("bp-icon-angle-down");
             $(this)
                 .find("i")
-                .removeClass("bp-icon-plus")
-                .addClass("bp-icon-cross");
+                .removeClass("bp-icon-angle-down")
+                .addClass("bp-icon-angle-up");
             $(".bp-accordion-set > a").removeClass("active");
             $(this).addClass("active");
-            $(".bp-accordion-body").slideUp(200);
+            $(".bp-accordion-body").slideUp(300);
             $(this)
                 .siblings(".bp-accordion-body")
-                .slideDown(200);
+                .slideDown(300);
         }
     });
-})
+});
