d3c3a5c137070657432810b7992d43434deecec7
Tey Liang Tai <tey_liang_tai@tech.gov.sg>
2018-07-26 13:34:07 +0800

    [sidenav]add sticky sidenav

--- assets/js/sideNav-offset.js
+++ assets/js/sideNav-offset.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').stickySidebar({
-        topSpacing: 40,
-        bottomSpacing: 40,
+        topSpacing: 170,
+        bottomSpacing: 0,
         containerSelector: '.has-side-nav',
         innerWrapperSelector: '.sidebar__inner'
     });
+    console.log($('.sidenav'))
 });
