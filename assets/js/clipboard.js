ffabfb7a657e52f9cca424dbafdfac5bfa6b2337
Tey Liang Tai <tey_liang_tai@tech.gov.sg>
2018-11-07 14:23:15 +0800

    add copy snippet buttons to all components

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
+    var highlights = $('.language-html');
+    highlights.each(function(key, value){
+        $(this).addClass('highlight' + key)
+    });
+
     var clipboard = new ClipboardJS('.clipboard-btn');
     clipboard.on('success',function(e){
-        console.info('Text:', e.text);
         e.clearSelection();
     });
 });
