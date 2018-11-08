e5389e78de060bdf67155a78ba8004a29ef19ebc
Tey Liang Tai <tey_liang_tai@tech.gov.sg>
2018-11-08 17:24:50 +0800

    update dropdown for components

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -6,6 +6,13 @@ $( document ).ready(function() {
 
     var clipboard = new ClipboardJS('.clipboard-btn');
     clipboard.on('success',function(e){
+        console.log(e)
+        var originalText = e.trigger.innerHTML;
         e.clearSelection();
+        e.trigger.textContent = 'Copied';
+        window.setTimeout(function() {
+            e.trigger.textContent = originalText;
+        }, 1000);
+
     });
 });
