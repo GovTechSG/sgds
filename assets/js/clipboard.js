65144c16d0cd5f1ea38ea6b23bc217f9d12a4a4b
Tey Liang Tai <tey_liang_tai@tech.gov.sg>
2018-11-12 15:03:27 +0800

    fix sidenav padding bottom, update masthead mobile view, added code snippets copy feedback

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -6,12 +6,14 @@ $( document ).ready(function() {
 
     var clipboard = new ClipboardJS('.clipboard-btn');
     clipboard.on('success',function(e){
-        console.log(e)
+
         var originalText = e.trigger.innerHTML;
         e.clearSelection();
         e.trigger.textContent = 'Copied';
+        e.trigger.classList.add('is-success');
         window.setTimeout(function() {
             e.trigger.textContent = originalText;
+            e.trigger.classList.remove('is-success');
         }, 1000);
 
     });
