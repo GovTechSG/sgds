92294e56a89f606126efe39eb3f9a8e5e3d06666
Tey Liang Tai <tey_liang_tai@tech.gov.sg>
2019-04-08 10:01:16 +0800

    SGDS-32 [Variant Components] Isomer's Side Navigation

--- sgds-govtech/js/sgds-sidenav.js
+++ sgds-govtech/js/sgds-sidenav.js
@@ -2,14 +2,18 @@ initSecondLevelNavInteraction();
 
 function initSecondLevelNavInteraction() {
     var secondLevelNavHeaderArray = document.querySelectorAll("li.second-level-nav-header");
-    for (var index = 0; index < secondLevelNavHeaderArray.length; index++) {
+    var secondLevelDiv_all = document.querySelectorAll('.second-level-nav-div');
 
+    for (var index = 0; index < secondLevelNavHeaderArray.length; index++) {
         function secondLevelNavClosure() {
             var closureIndex = index;
+            console.log(secondLevelDiv_all[index]);
+            // secondLevelDiv_all[index].classList.add("is-hidden");
             function toggleSecondLevelNavDiv() {
                 // Get the icon of the second-level-nav-header
                 var secondLevelNavIcon = secondLevelNavHeaderArray[closureIndex].getElementsByTagName("I")[0];
                 var secondLevelDiv = document.getElementsByClassName("second-level-nav-div")[closureIndex];
+
                 if (secondLevelDiv.classList.contains("is-hidden")) {
                     secondLevelNavIcon.classList.remove("sgds-icon-chevron-down");
                     secondLevelNavIcon.classList.add("sgds-icon-chevron-up");
@@ -19,8 +23,10 @@ function initSecondLevelNavInteraction() {
                     secondLevelNavIcon.classList.remove("sgds-icon-chevron-up");
                     secondLevelNavIcon.classList.add("sgds-icon-chevron-down");
                 }
+
             }
             return toggleSecondLevelNavDiv;
+
         }
 
         secondLevelNavHeaderArray[index].addEventListener("click", secondLevelNavClosure());
