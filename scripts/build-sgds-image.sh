e12ba743cc0f9b257a507887abe6b8847c9890b6
Seng-Siong Lim <lim_seng_siong@tech.gov.sg>
2019-04-10 17:08:57 +0800

    update build script

--- scripts/build-sgds-image.sh
+++ scripts/build-sgds-image.sh
@@ -1,9 +1,15 @@
 version=$(cat version)
 IMAGE_NAME="sgds/portal:"$version
-
+FILE_NAME="sgds-portal-v"$version".tar.gz"
 if [ $1 = "--delete-dangling" -o $1 = "-d" ]; then
     echo "Cleaning up dangling images..."
     docker images -q --filter dangling=true | xargs docker rmi
 fi
 
 docker build --no-cache --tag $IMAGE_NAME .
+
+# Ensure that the directory exists.
+mkdir images
+
+# Save image out to tar.gz for scp
+docker save $IMAGE_NAME | gzip > $FILE_NAME
