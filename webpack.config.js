e6de8e117cdd859b8e3c3a6d1097a46bca72e7f9
Wei Junyu <weijunyu@users.noreply.github.com>
2019-09-24 13:50:55 +0800

    Cleaned up js directory structure, improved getting-started content

--- webpack.config.js
+++ webpack.config.js
@@ -6,104 +6,105 @@ const ImageminPlugin = require("imagemin-webpack-plugin").default;
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = env => {
-    if (!env) env = {};
-    // This object only for SGDS Main Library
-    // Site config (Vue, ImageMin etc) is below this object
-    let config = {
-        mode: env.production ? "production" : "development",
-        entry: {
-            "js/sgds": "./sgds/index.js"
+  if (!env) env = {};
+  // This object only for SGDS Main Library
+  // Site config (Vue, ImageMin etc) is below this object
+  let config = {
+    mode: env.production ? "production" : "development",
+    entry: {
+      "js/sgds": "./sgds/js/index.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules\/(?!(sticky-sidebar))/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread"]
+            }
+          }
         },
-        output: {
-            filename: "[name].js",
-            path: path.resolve(__dirname)
+        {
+          // sass -> css -> extract to dist/css
+          test: /sgds\.s(a|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: "/"
+              }
+            },
+            {
+              loader: "css-loader",
+              options: {
+                url: false
+              }
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
         },
-        module: {
-            rules: [
-                {
-                    test: /\.m?js$/,
-                    exclude: /node_modules\/(?!(sticky-sidebar))/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"],
-                            plugins: ["@babel/plugin-proposal-object-rest-spread"]
-                        }
-                    }
-                },
-                {
-                    // sass -> css -> extract to dist/css
-                    test: /sgds\.s(a|c)ss$/,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                            options: {
-                                publicPath: "/"
-                            }
-                        },
-                        {
-                            loader: "css-loader",
-                            options: {
-                                url: false
-                            }
-                        },
-                        {
-                            loader: "sass-loader"
-                        }
-                    ]
-                },
-                {
-                    // fonts -> file loader to dist/fonts
-                    test: /sgds-icons\.(svg|ttf|woff)$/,
-                    use: [
-                        {
-                            loader: "file-loader",
-                            options: {
-                                name: "[name].[ext]",
-                                outputPath: "fonts"
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                moduleFilename: ({ name }) => `${name.replace("js", "css")}.css`
-            })
-        ]
-    };
+        {
+          // fonts -> file loader to dist/fonts
+          test: /sgds-icons\.(svg|ttf|woff)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "fonts"
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        moduleFilename: ({ name }) => `${name.replace("js", "css")}.css`
+      })
+    ]
+  };
 
-    if (env.production) {
-        config.plugins.push(new OptimizeCssAssetsPlugin());
-        config.devtool = "source-map";
-    }
+  if (env.production) {
+    config.plugins.push(new OptimizeCssAssetsPlugin());
+    config.devtool = "source-map";
+  }
 
-    if (env.site) {
-        config.entry["apps/build/vue-search-app"] = "./apps/src/search/vue-search-app.js";
-        config.module.rules.push({
-            test: /\.vue$/,
-            loader: "vue-loader"
-        });
-        config.plugins.push(
-            ...[
-                new CopyWebpackPlugin([
-                    {
-                        from: "assets/uncompressed_images",
-                        to: "assets/img"
-                    }
-                ]),
-                // ImageminWebpackPlugin must be placed after CopyWebpackPlugin
-                // (or any other plugins that deal with images) in plugins array.
-                new ImageminPlugin({
-                    pngquant: {
-                        quality: "95-100"
-                    }
-                }),
-                new VueLoaderPlugin()
-            ]
-        );
-    }
+  if (env.site) {
+    config.entry["apps/build/vue-search-app"] =
+      "./apps/src/search/vue-search-app.js";
+    config.module.rules.push({
+      test: /\.vue$/,
+      loader: "vue-loader"
+    });
+    config.plugins.push(
+      ...[
+        new CopyWebpackPlugin([
+          {
+            from: "assets/uncompressed_images",
+            to: "assets/img"
+          }
+        ]),
+        // ImageminWebpackPlugin must be placed after CopyWebpackPlugin
+        // (or any other plugins that deal with images) in plugins array.
+        new ImageminPlugin({
+          pngquant: {
+            quality: "95-100"
+          }
+        }),
+        new VueLoaderPlugin()
+      ]
+    );
+  }
 
-    return config;
+  return config;
 };
