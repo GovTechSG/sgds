726444911c0acb37613ec53241c76c113bb5c443
rav3 <rav3tey@gmail.com>
2022-03-15 09:43:31 +0800

    style(homepage): update home page

--- src/sgds.js
+++ src/sgds.js
@@ -1,4 +1,4 @@
 import "../sgds/sgds.scss";
 import Bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
-const masthead = require('./sgds-masthead');
-masthead();
+// const masthead = require('./sgds-masthead');
+// masthead();
