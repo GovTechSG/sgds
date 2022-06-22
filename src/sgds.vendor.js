9dc0f5f7e9db5600db790f1c2144267fb037b2ce
rav3 <rav3tey@gmail.com>
2022-06-22 15:54:41 +0800

    offcanvas(*): update menu, remove scroll background navbar

--- src/sgds.vendor.js
+++ src/sgds.vendor.js
@@ -2,17 +2,17 @@ import { gsap } from 'gsap';
 import {ScrollTrigger} from 'gsap/dist/ScrollTrigger';
 // import Shuffle from 'shufflejs';
 
-gsap.registerPlugin(ScrollTrigger);
-gsap.to(".sgds.navbar",{
-    scrollTrigger: {
-        trigger: ".sgds.navbar",
-        start: "10px top",
-        scrub: 1
-    },
-    backgroundColor: "rgba(244, 246, 250, 0.6)",
-    backdropFilter: "blur(10px)",
-    duration: 0.35
-});
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.to(".sgds.navbar",{
+//     scrollTrigger: {
+//         trigger: ".sgds.navbar",
+//         start: "10px top",
+//         scrub: 1
+//     },
+//     backgroundColor: "rgba(244, 246, 250, 0.6)",
+//     backdropFilter: "blur(10px)",
+//     duration: 0.35
+// });
 
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navbarCollapse = document.querySelector(".navbar-collapse");
