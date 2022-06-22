4153047c1a382e1336af6896cb88ff5d1ce7aeb8
rav3 <rav3tey@gmail.com>
2022-06-22 16:33:13 +0800

    tooltip(*): activate tooltip component

--- src/sgds.bundle.js
+++ src/sgds.bundle.js
@@ -1,1 +1,7 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+window.bootstrap = require("bootstrap")
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
