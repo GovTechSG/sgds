91ae780594c3faca888bbb177d401275af9b0745
LT <rav3tey@gmail.com>
2023-10-23 16:39:09 +0800

    doc(code-snippet): update react and storybook link

--- src/sgds.vendor.js
+++ src/sgds.vendor.js
@@ -1,4 +1,4 @@
-import ClipboardJS from 'clipboard';
+// import ClipboardJS from 'clipboard';
 import _ from 'lodash';
 
 const navbarToggler = document.querySelector(".navbar-toggler");
@@ -12,16 +12,39 @@ document.addEventListener("devConsoleWidgetToggle", function (event) {
     }
 });
 
-const clipboard = new ClipboardJS('.btn-copy', {
-    target: trigger => trigger.closest('.code-snippet').querySelector('.highlight')
-});
+// const clipboard = new ClipboardJS('.btn-copy', {
+//     target: trigger => trigger.closest('.code-snippet').querySelector('.highlight')
+// });
 
-clipboard.on('success', function(e) {
-    const copyBtn = e.trigger;
+// clipboard.on('success', function(e) {
+//     const copyBtn = e.trigger;
+//     const originalText = copyBtn.innerText;
+//     copyBtn.innerText = "Copied";
+//     _.delay(function(){
+//         copyBtn.innerText = originalText;
+//     },2000);
+//     e.clearSelection();
+// });
+
+var tabEl = document.querySelectorAll('button[data-bs-toggle="tab"]')
+let currentTab = 'html';
+tabEl.forEach(function(el){
+    el.addEventListener('shown.bs.tab', function (e) {
+    console.log(e.target.attributes[6].nodeValue) // newly activated tab
+    currentTab = e.target.attributes[6].nodeValue
+    // event.relatedTarget // previous active tab
+    })
+})
+
+const copyBtn = document.querySelector(".btn-copy");
+copyBtn.addEventListener('click', () => {
+    console.log(currentTab)
     const originalText = copyBtn.innerText;
-    copyBtn.innerText = "Copied";
-    _.delay(function(){
-        copyBtn.innerText = originalText;
-    },2000);
-    e.clearSelection();
+    navigator.clipboard.writeText(document.querySelector(".code-" + currentTab).firstChild.textContent)
+    .then(() => {
+        copyBtn.innerText = "Copied " + currentTab;
+        _.delay(function(){
+            copyBtn.innerText = originalText;
+        },2000);
+    })
 });
