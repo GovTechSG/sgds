ec55c666f8ef4dc1275cbc30b616adebfcb85b0d
clukhei <kheiboardwarrior@gmail.com>
2023-11-23 21:20:36 +0800

    refactor(tooltip): move tooltip trigger script from lib to portal

--- portal/webpack.config.js
+++ portal/webpack.config.js
@@ -5,12 +5,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    "sgds": './src/sgds.js',
-    "sgds.util": './src/sgds.util.js',
-    "assets/js/bundle": './src/sgds.bundle.js',
-    "assets/js/vendor": './src/sgds.vendor.js',
-    "../portal/assets/js/bundle": './src/sgds.vendor.js',
-    "../portal/assets/js/vendor": './src/sgds.vendor.js',
+    "assets/js/tooltip.bundle": './js/tooltip.js',
   },
   devtool: 'source-map',
   mode: "development",
@@ -18,53 +13,6 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname)
   },
-  module: {
-    rules: [
-      {
-        // sass -> css -> extract to dist/css
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      }
-    ]
-      
-  },
-  // minimiser only runs in --mode=production
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: './css/[name].css'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '../portal/css/[name].css'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { 
-          from: "assets/uncompressed_images/", 
-          to: "assets/img/",
-        }
-      ],
-   
-    }),
   ],
 };
