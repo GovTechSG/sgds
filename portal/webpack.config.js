f291303fa0e8dac77a7aceec456a499554ea9c0f
clukhei <kheiboardwarrior@gmail.com>
2024-01-03 15:39:41 +0800

    build(*): compressor for images

--- portal/webpack.config.js
+++ portal/webpack.config.js
@@ -1,15 +1,68 @@
 const path = require("path");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: {
-    "assets/js/portal-polyfills.bundle": './js/portal-polyfills.js',
-  },
-  devtool: 'source-map',
-  mode: "development",
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname)
-  },
-  plugins: [
-  ],
-};
+
+module.exports = (env, argv) => ({
+    entry: {
+        "assets/js/portal-polyfills.bundle": "./js/portal-polyfills.js",
+    },
+    devtool: "source-map",
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname),
+    },
+    plugins: argv.mode === "production" && compressImagePlugins
+});
+
+const compressImagePlugins = [
+  new CopyPlugin({
+      patterns: [
+          {
+              from: "assets/uncompressed-images",
+              to({ context, absoluteFilename }) {
+                  return "assets/img";
+              },
+          },
+      ],
+  }),
+  new ImageMinimizerPlugin({
+      minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+              // Lossless optimization with custom option
+              // Feel free to experiment with options for better result for you
+              plugins: [
+                  ["gifsicle", { interlaced: true }],
+                  ["jpegtran", { progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }],
+                  // Svgo configuration here https://github.com/svg/svgo#configuration
+                  [
+                      "svgo",
+                      {
+                          plugins: [
+                              {
+                                  name: "preset-default",
+                                  params: {
+                                      overrides: {
+                                          removeViewBox: false,
+                                          addAttributesToSVGElement: {
+                                              params: {
+                                                  attributes: [
+                                                      {
+                                                          xmlns: "http://www.w3.org/2000/svg",
+                                                      },
+                                                  ],
+                                              },
+                                          },
+                                      },
+                                  },
+                              },
+                          ],
+                      },
+                  ],
+              ],
+          },
+      },
+  }),
+]
